6a37d410019613774e036b026989d1f94345d579
AdLKL <lkl88adnane@gmail.com>
2025-02-19 18:33:07 +0100

    Solved 3 Problems

---
+++ src/BitPlusPlus.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class BitPlusPlus {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int x = 0;
+        for(int i=0; i<n; i++) {
+            String s = sc.next();
+            for(char c : s.toCharArray()) {
+                if(c == 'X') continue;
+                if(c == '+') {
+                    x++;
+                    break;
+                }
+                else {
+                    x--;
+                    break;
+                }
+            }
+        }
+        System.out.println(x);
+    }
+}
---
+++ src/NextRound.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class NextRound {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int[] scores = new int[n];
+        for(int i=0; i<n; i++) {
+            scores[i] = sc.nextInt();
+        }
+        int thresh = scores[k-1];
+        int result = 0;
+        for(int num : scores) {
+            if(num >= thresh && num > 0) result++;
+        }
+        System.out.println(result);
+    }
+}
